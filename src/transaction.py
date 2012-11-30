603263ec49d28ea7841a2e2afb00619dbc78b4d3
Sushant Bhadkamkar <mailsushant@gmail.com>
2012-11-30 15:39:31 -0500

    Transaction class with tests

---
+++ src/transaction.py
@@ -0,0 +1,33 @@
+class TransactionType:
+  READ_ONLY  = 0
+  READ_WRITE = 1
+
+class TransactionStatus:
+  ACTIVE  = 0
+  ABORTED = 1
+  WAITING = 2
+
+
+class Transaction:
+  def __init__(self, txn_id, ts, txn_status=TransactionStatus.ACTIVE, txn_type=TransactionType.READ_WRITE):
+    self._id     = txn_id
+    self._ts     = ts   # Timestamp of transaction
+    self._status = txn_status
+    self._type   = txn_type
+
+
+  def get_ts(self):
+    return self._ts
+
+
+  def get_status(self):
+    return self._status
+
+
+  def get_type(self):
+    return self._type
+
+
+  def wait(self):
+    if self._status != TransactionStatus.WAITING:
+      self._status = TransactionStatus.WAITING
